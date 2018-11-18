05a54c12a267c9884519c42a8fe65c3ef216d1cb
AleksandarTheBoulderRankovic <mi16111@alas.matf.bg.ac.rs>
2018-11-18 15:20:17 +0100

    Kod za drugu nedelju

--- pocetak.c
+++ pocetak.c
@@ -67,14 +67,14 @@ static void on_display(void)
     gluPerspective(
             60,
             window_width/(float)window_height,
-            1, 25);
+            1, 1500);
 
     /* Podesavamo tacku pogleda. */
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(
-            2, 2, 2,
-            0, 0, 0,
+            20, 35, 20,
+            0, 1, 0,
             0, 1, 0
         );
 
@@ -82,19 +82,71 @@ static void on_display(void)
     	/* X osa */
         glColor3f(1,0,0); 
         glVertex3f(0,0,0);
-        glVertex3f(8,0,0);
+        glVertex3f(100,0,0);
         
         /* Y osa */
         glColor3f(0,0,1);
         glVertex3f(0,0,0);
-        glVertex3f(0,8,0);
+        glVertex3f(0,100,0);
         
         /* Z osa */
         glColor3f(0.25,0.25,0.25);
         glVertex3f(0,0,0);
-        glVertex3f(0,0,8);
+        glVertex3f(0,0,100);
     glEnd();
+
+    /*Pod naprevljen od kocke translirane tako da
+    njena gornja ivica bude u centru koordinatnog 
+    sistema*/
+	glColor3f(0,0.4,0);
+	//glRotatef(45, 0, 1, 0);
+	glPushMatrix();
+	glTranslatef(0, -1, 0);
+	glScalef(30, 0, 30);
+	glutSolidCube(1);
+	glPopMatrix();
+
+	/*Desni zid napravljen od kocke koja je translirana
+	do desne ivice poda a zatim skalirana po celoj duzini z-ose
+	poda i po y-osi kako bi dobio na visini*/
+	glColor3f(0,0.2,0);
+	glPushMatrix();
+	glTranslatef(15.5, 0, 0);
+	glScalef(0, 8, 31);
+	glutSolidCube(1);
+	glPopMatrix();
+
+	/*Levi zid napravljen od kocke koja je translirana
+	do leve ivice poda a zatim skalirana po celoj duzini z-ose
+	poda i po y-osi kako bi dobio na visini*/
+	glColor3f(0,0.2,0);
+	glPushMatrix();
+	glTranslatef(-15.5, 0, 0);
+	glScalef(0, 8, 31);
+	glutSolidCube(1);
+	glPopMatrix();
+
+	/*Gornji zid*/
+	glColor3f(0.251,0.251,0.251);
+	glPushMatrix();
+	glTranslatef(0, 0, 15.5);
+	glScalef(31, 8, 0);
+	glutSolidCube(1);
+	glPopMatrix();
+
+	/*Donji zid*/
+	glColor3f(0.251,0.251,0.251);
+	glPushMatrix();
+	glTranslatef(0, 0, -15.5);
+	glScalef(31, 8, 0);
+	glutSolidCube(1);
+	glPopMatrix();
+
+
+	/*Zmijica - samo pocetni objekat koji treba skalirati*/
+	glColor3f(0.5, 1, 0);
+	glutSolidCube(1);
+
     /* Nova slika se salje na ekran. */
     glutSwapBuffers();
 }
- 
